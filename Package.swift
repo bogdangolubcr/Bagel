6b77ebf8a6ee2c9ca9ba9b48cb41c7cd3e67f153
bogdangolubcr <bogdan.golub@cleveroad.com>
2020-10-28 09:50:51 +0200

    Update Package.swift
    d

--- Package.swift
+++ Package.swift
@@ -1,21 +1,2 @@
 // swift-tools-version:5.0
-import PackageDescription
 
-let package = Package(
-    name: "Bagel",
-    products: [
-        .library(name: "Bagel", targets: ["Bagel"])
-    ],
-    dependencies: [
-        .package(url: "https://github.com/bogdangolubcr/CocoaAsyncSocket.git", .branch("master")),
-    ],
-    targets: [
-        .target(
-            name: "Bagel",
-            dependencies: ["CocoaAsyncSocket"],
-            path: "iOS/Source",
-            publicHeadersPath: "."
-        )
-    ],
-    swiftLanguageVersions: [.v5]
-)
